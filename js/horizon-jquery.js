c0638034797df640baf84e065415dcc5ae2748e8
lawrence <lawrence.t.yen@gmail.com>
2019-06-16 11:28:15 -0700

    upload to git

---
+++ js/horizon-jquery.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('.nav');
+   var offset = startchange.offset();
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $('.header-row').css('background-color', 'rgba(255,255,255,0.9)');
+       } else {
+          $('.header-row').css('background-color', 'transparent');
+       }
+   });
+});
+
+
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
